c9edd4f8c2d0b1e70fabb98e684db3fea906546f
Juan Diego <silgajuandiego@gmail.com>
2018-06-16 06:39:11 -0500

    [Add-DB] Add automatic septup.js database

--- platziverse-db/setup.js
+++ platziverse-db/setup.js
@@ -1,42 +1,35 @@
 'use strict'
 
-const debug = require('debug')('platziverse:db:setup')
-const inquirer = require('inquirer')
-const chalk = require('chalk')
 const db = require('./')
-
-const prompt = inquirer.createPromptModule()
+const debug = require('debug')('platziverse:db:setup')
+const chalk = require('chalk')
 
 async function setup () {
-  const answer = await prompt([
-    {
-      type: 'confirm',
-      name: 'setup',
-      message: 'This will destroy your database, are you sure?'
+  const value = process.argv.filter(arg => arg === '-y' || arg === '--yes')[0]
+
+  if (value === '-y' || value === '--yes') {
+    const config = {
+      database: process.env.DB_NAME || 'platziverse',
+      username: process.env.DB_USER || 'platzi',
+      password: process.env.DB_PASS || 'platzi',
+      host: process.env.DB_HOST || 'localhost',
+      dialect: process.env.DB_USAGE || 'postgres',
+      setup: true,
+      logging: m => debug(m)
     }
-  ])
 
-  if (!answer.setup) {
-    return console.log('Nothing happened :)')
-  }
+    await db(config).catch(handleFatalError)
 
-  const config = {
-    database: process.env.DB_NAME || 'platziverse',
-    username: process.env.DB_USER || 'platzi',
-    password: process.env.DB_PASS || 'platzi',
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'postgres',
-    logging: s => debug(s),
-    setup: true
+    console.log('Success!')
+    process.exit(0)
+  } else {
+    console.log(chalk.green(`The database is ok!`))
   }
-  await db(config).catch(handleFatalError)
-  console.log('Success!')
-  process.exit(0)
 }
 
 function handleFatalError (err) {
-  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
-  console.error(err.stack)
+  console.error(chalk.red(err.message))
+  console.error(chalk.red(err.stack))
   process.exit(1)
 }
 
