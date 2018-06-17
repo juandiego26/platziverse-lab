5cd6375b9df789e7e40e15196e37296061c691df
Juan Diego <silgajuandiego@gmail.com>
2018-06-16 21:07:17 -0500

    [Add-MQTT] Add events published, connected and disconnected

--- platziverse-mqtt/server.js
+++ platziverse-mqtt/server.js
@@ -6,6 +6,8 @@ const redis = require('redis')
 const chalk = require('chalk')
 const db = require('platziverse-db')
 
+const { parsePayload } = require('./utils')
+
 const backend = {
   type: 'redis',
   redis,
@@ -27,21 +29,102 @@ const config = {
 }
 // instanciar el servidor
 const server = new mosca.Server(settings)
+const clients = new Map()
 
 let Agent, Metric
 
 // Evento para cuando un cliente se conecta al servidor
 server.on('clientConnected', client => {
   debug(`Client Connected: ${client.id}`)
+  clients.set(client.id, null)
 })
 // Evento para cuando un cliente se desconecta del servidor
-server.on('clientDisconnected', client => {
+server.on('clientDisconnected', async (client) => {
   debug(`Client Disconnected: ${client.id}`)
+  const agent = clients.get(client.id)
+
+  if (agent) {
+    // Mark Agent as Disconnected
+    agent.connected = false
+
+    try {
+      await Agent.createOrUpdate(agent)
+    } catch (e) {
+      return handleError(e)
+    }
+
+    // Delete Agent from Clients List
+    clients.delete(client.id)
+
+    server.publish({
+      topic: 'agent/disconnected',
+      payload: JSON.stringify({
+        agent: {
+          uuid: agent.uuid
+        }
+      })
+    })
+    debug(`Client (${client.id}) associated to Agent (${agent.uuid}) marked as disconneted`)
+  }
 })
 // Evento mas importante cuando hay un mensaje publicado en el servidor
-server.on('published', (packet, client) => {
+server.on('published', async (packet, client) => {
   debug(`Received: ${packet.topic}`)
-  debug(`Payload: ${packet.payload}`)
+
+  switch (packet.topic) {
+    case 'agent/connected':
+    case 'agent/disconnected':
+      debug(`Payload: ${packet.payload}`)
+      break
+    case 'agent/message':
+      debug(`Payload: ${packet.payload}`)
+
+      const payload = parsePayload(packet.payload)
+
+      if (payload) {
+        payload.agent.connected = true
+
+        let agent
+        try {
+          agent = await Agent.createOrUpdate(payload.agent)
+        } catch (e) {
+          return handleError(e)
+        }
+
+        debug(`Agent ${agent.uuid} saved`)
+
+        // Notify Agent is connected
+        if (!clients.get(client.id)) {
+          clients.set(client.id, agent)
+          server.publish({
+            topic: 'agent/connected',
+            payload: JSON.stringify({
+              agent: {
+                uuid: agent.uuid,
+                name: agent.name,
+                hostname: agent.hostname,
+                pid: agent.pid,
+                connected: agent.connected
+              }
+            })
+          })
+        }
+
+        // Store Metrics
+        for (let metric of payload.metrics) {
+          let m
+
+          try {
+            m = await Metric.create(agent.uuid, metric)
+          } catch (e) {
+            return handleError(e)
+          }
+
+          debug(`Metric ${m.id} saved on agent ${agent.uuid}`)
+        }
+      }
+      break
+  }
 })
 // eventEmitter evento cuando el servidor este corriendo
 server.on('ready', async () => {
@@ -60,6 +143,12 @@ function handleFatalError (err) {
   console.error(err.stack)
   process.exit(1)
 }
+
+function handleError (err) {
+  console.error(`${chalk.red('[error]')} ${err.message}`)
+  console.error(err.stack)
+}
+
 // buena practica de desarrollo cuando hay algun tipo de excepcion
 // que no fue manejada, para pasar a nivel del proceso
 process.on('uncaughtException', handleFatalError)
