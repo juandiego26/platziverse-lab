8870e2173fb44e220c31bdfd20c29bd327f9274d
Juan Diego <silgajuandiego@gmail.com>
2018-06-20 16:57:19 -0500

    [Add-Auth-API] Implement Authorization permissions

--- platziverse-api/api.js
+++ platziverse-api/api.js
@@ -4,6 +4,7 @@ const debug = require('debug')('platziverse:api:routes')
 const express = require('express')
 const asyncify = require('express-asyncify')
 const auth = require('express-jwt')
+const guard = require('express-jwt-permissions')()
 const db = require('platziverse-db')
 
 const config = require('./config')
@@ -72,7 +73,7 @@ api.get('/agent/:uuid', auth(config.auth), async (req, res, next) => {
 })
 
 // ruta get retorna metricas tiene reportadas un agente especifíco
-api.get('/metrics/:uuid', auth(config.auth), async (req, res, next) => {
+api.get('/metrics/:uuid', auth(config.auth), guard.check(['metrics:read']), async (req, res, next) => {
   const { uuid } = req.params
 
   debug(`request to /metrics/${uuid}`)
