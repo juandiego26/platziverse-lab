e201b0a190f4467ad91e2da4867ed15e353f42f1
Juan Diego <silgajuandiego@gmail.com>
2018-06-22 21:18:29 -0500

    [Add-web] integrate agent component with Socket.io

--- platziverse-web/client/app.vue
+++ platziverse-web/client/app.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <agent uuid="78f37155-7336-4cf7-863a-7a58d6406c80" :socket="socket"></agent>
     <agent
       v-for="agent in agents"
       :uuid="agent.uuid"
-      :key="agent.uuid">
+      :key="agent.uuid"
+      :socket="socket">
     </agent>
     <p v-if="error">{{error}}</p>
   </div>
@@ -19,6 +19,7 @@
 </style>
 
 <script>
+const request = require('request-promise-native')
 const io = require('socket.io-client')
 const socket = io()
 
@@ -36,7 +37,30 @@ module.exports = {
   },
 
   methods: {
-    initialize () {
+    async initialize () {
+      const options = {
+        method: 'GET',
+        url: `http://localhost:8080/agents`,
+        json: true
+      }
+
+      let result
+      try {
+        result = await request(options)
+      } catch (e) {
+        this.error = e.error.error
+        return
+      }
+
+      this.agents = result
+
+      socket.on('agent/connected', payload => {
+        const { uuid } = payload.agent
+        const existing = this.agents.find(a => a.uuid === uuid)
+        if (!existing) {
+          this.agents.push(payload.agent)
+        }
+      })
     }
   }
 }
